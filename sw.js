debcb26cfc27a2cd650ae237d0817a62129982ab
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2025-10-29 12:12:34 +0100

    Corrige l'affichage des panneaux repliables

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'belgfr-scoreboard-v12';
+const CACHE_NAME = 'belgfr-scoreboard-v13';
 const ASSETS = [
   './',
-  './?v=0.12',
+  './?v=0.13',
   './index.html',
   './styles.css',
   './app.js',
